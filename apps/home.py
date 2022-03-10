11a48cac2a4f52395022bb45f8466274496d0028
dominik3335 <97000592+dominik3335@users.noreply.github.com>
2022-03-10 12:26:05 +0100

    Update home.py
    changed st.write to st.header to increase font size

--- apps/home.py
+++ apps/home.py
@@ -6,18 +6,18 @@ def app():
 
     st.title("cycle_safe(berlin) - helping to keep you and your bike safe")
 
-    st.write("How can we use Machine Learning to mitigate")
-    st.write("1. Bike theft risk")
-    st.write("2. Bike accident risk")
+    st.header("How can we use Machine Learning to mitigate")
+    st.header("1. Bike theft risk")
+    st.header("2. Bike accident risk")
 
     #image = Image.open('./images/BikeTheft.png')
     st.image("./images/BikeTheft.png", caption='bike theft')
 
     st.markdown("### The Team")
 
-    st.write("Hitoshi Michinaka [Linkedin](https://www.linkedin.com/in/hmichinaka/)")
-    st.write("Dominik Abratanski [Linkedin](https://www.linkedin.com/in/dominikabratanski/)")
-    st.write("Lukas Hartung [Linkedin](https://www.linkedin.com/in/lukas-h-0438578b/)")
-    st.write("Marlon Deus [Linkedin](https://www.linkedin.com/in/marlon-deus-0a37031b4/)")
-    st.write("Paul Roberts [Linkedin](https://www.linkedin.com/in/paul-roberts-871a6790/)")
-    st.write("Jakob Hohenstein [Linkedin](https://www.linkedin.com/in/jakob-hohenstein-53667914a/)")
+    st.header("Hitoshi Michinaka [Linkedin](https://www.linkedin.com/in/hmichinaka/)")
+    st.header("Dominik Abratanski [Linkedin](https://www.linkedin.com/in/dominikabratanski/)")
+    st.header("Lukas Hartung [Linkedin](https://www.linkedin.com/in/lukas-h-0438578b/)")
+    st.header("Marlon Deus [Linkedin](https://www.linkedin.com/in/marlon-deus-0a37031b4/)")
+    st.header("Paul Roberts [Linkedin](https://www.linkedin.com/in/paul-roberts-871a6790/)")
+    st.header("Jakob Hohenstein [Linkedin](https://www.linkedin.com/in/jakob-hohenstein-53667914a/)")
